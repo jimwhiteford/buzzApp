e26a3ebef7fa120252601cdf651cd6ff4c047acc
Jim <jim.whiteford@hotmail.com>
2024-06-19 17:20:52 +0100

    creating logo and text

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,11 @@ export default function App() {
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{height: '100%'}}>
         <View className="w-full justify-center items-center h-full px-4">
-          <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode='contain' />
+          <View className="flex-row items-center">
+            <Image source={images.hLogo} className="w-[105px] h-[60px] -mr-6" resizeMode='contain' />
+            <Text className="text-white text-5xl font-semibold">Buzz</Text>
+          </View>
+          
           <Image source={images.cards} className="w-[380px] h-[300px]" resizeMode='contain' />
         </View>
       </ScrollView>
