8d4dc0239f5d106cb4f3fadffbe83cd71d5eb464
Jim <jim.whiteford@hotmail.com>
2024-06-20 12:52:34 +0100

    finished welcome page, with custom button component

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import {  ScrollView, Text, View, Image } from 'react-native';
-import { Link } from 'expo-router'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import {images} from '../constants'
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, Text, View, Image } from "react-native";
+import { Redirect, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
-      <ScrollView contentContainerStyle={{height: '100%'}}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className="w-full justify-center items-center h-full px-4">
           <View className="flex-row items-center">
-            <Image source={images.hLogo} className="w-[105px] h-[60px] -mr-6" resizeMode='contain' />
-            <Text className="text-white text-5xl font-semibold">Buzz</Text>
+            <Image
+              source={images.hLogo}
+              className="w-[105px] h-[60px] -mr-6"
+              resizeMode="contain"
+            />
+            <Text className="text-white text-5xl font-bold">Buzz</Text>
           </View>
-          
-          <Image source={images.cards} className="w-[380px] h-[300px]" resizeMode='contain' />
+          <Image
+            source={images.beePhoto1}
+            className="w-[480px] h-[300px] "
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Effortless Apiary Managment with{" "}
+              <Text className="text-secondary-200">Buzz</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-1.5 right-24"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Technology meets tradition: An easy to use QR code scanning
+            system to track and manage your hives.
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("sign-in")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
-
--- constants/images.js
+++ constants/images.js
@@ -6,5 +6,7 @@ import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
 import hLogo from "../assets/images/honeyLogo.png"
+import beePhoto from "../assets/images/bee-photos.png"
+import beePhoto1 from "../assets/images/bee-photos1.png"
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty, hLogo };
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, hLogo, beePhoto, beePhoto1 };
